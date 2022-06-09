b3c1a2ad6202f06978ee8079a016b82c99840299
Ajith Ronson Dsouza <36181901+ajithdsouza221@users.noreply.github.com>
2022-06-09 14:14:02 +0530

    Add files via upload

---
+++ gcpaj/src/main/java/com/ihub/demo/GcpajApplication.java
@@ -0,0 +1,13 @@
+package com.ihub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpajApplication.class, args);
+	}
+
+}
---
+++ gcpaj/src/test/java/com/ihub/demo/GcpajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
